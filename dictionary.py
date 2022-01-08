acac7abfee401713cae02c57f13b68397bcb492b
arslanit1993 <97192790+arslanit1993@users.noreply.github.com>
2022-01-08 17:34:19 +0300

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+a = {
+   '1':["快乐","kuàilè","happy",],
+   '2':['错','cuò','wrong'],
+   '3':['阴','yīn','cloudy'],
+}
+
+
+def func(b):
+    correct = 0
+    incorrect = 0
+
+    while b <= len(a):
+        c = input(a[str(b)][0])
+        if c == a[str(b)][2]:
+            return func(b+1)
+                
+    
+    
+func(1)
